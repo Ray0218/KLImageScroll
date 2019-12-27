19bff1aa74f7ee47113a0be58aa0f79cf69d4c45
吴孔亮 <ray_ios@163.com>
2019-12-27 16:48:45 +0800

    init

---
+++ KLImageScroll/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  KLImageScroll
+//
+//  Created by WKL on 2019/12/27.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KLImageScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KLImageScroll
+//
+//  Created by WKL on 2019/12/27.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
